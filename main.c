18398afe45430cde2dbeec38a608f277d6687e03
Valentin OMNES <vomnes@student.42.fr>
2017-01-24 19:15:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -19,15 +19,25 @@ int ft_push_exit(int keycode, void *param)
     return (0);
 }
 
-int ft_draw_line_one(int x0, int y0, int x1, int y1, void *mlx, void *win)
+/*
+if (x1 - x0 < 0 && y1 - y0 >= 0)
+{
+    ft_swap(&x0, &x1);
+    ft_swap(&y0, &y1);
+}
+else if (x1 - x0 < 0 && y1 - y0 < 0)
+{
+    ft_swap(&x0, &x1);
+    ft_swap(&y0, &y1);
+}
+*/
+
+int ft_draw_line_slow(int x0, int y0, int x1, int y1, void *mlx, void *win)
 {
     int dx;
     int dy;
     int x;
 
-    ft_printf("======BEFORE=====\n");
-    ft_printf("x0: %4d y0: %4d\n", x0, y0);
-    ft_printf("x1: %4d y1: %4d\n", x1, y1);
     if (x1 - x0 < 0 && y1 - y0 >= 0)
     {
         ft_swap(&x0, &x1);
@@ -40,11 +50,6 @@ int ft_draw_line_one(int x0, int y0, int x1, int y1, void *mlx, void *win)
     }
     dx = x1 - x0;
     dy = y1 - y0;
-    ft_printf("======AFTER======\n");
-    ft_printf("x0: %4d y0: %4d\n", x0, y0);
-    ft_printf("x1: %4d y1: %4d\n", x1, y1);
-    ft_printf("dx: %4d dy: %4d\n", dx, dy);
-    ft_printf("\n");
     x = x0;
     while (x <= x1)
     {
@@ -54,6 +59,89 @@ int ft_draw_line_one(int x0, int y0, int x1, int y1, void *mlx, void *win)
     return (0);
 }
 
+int ft_draw_line_hard(int x0, int y0, int x1, int y1, void *mlx, void *win)
+{
+    int dx;
+    int dy;
+    int y;
+
+    if (x1 - x0 >= 0 && y1 - y0 <= 0)
+    {
+        ft_swap(&x0, &x1);
+        ft_swap(&y0, &y1);
+    }
+    else if (x1 - x0 < 0 && y1 - y0 < 0)
+    {
+        ft_swap(&x0, &x1);
+        ft_swap(&y0, &y1);
+    }
+    dx = x1 - x0;
+    dy = y1 - y0;
+    y = y0;
+    while (y <= y1)
+    {
+        mlx_pixel_put(mlx, win, x0 + (dx * (y - y0) / dy), y, 0x00FAAFF);
+        y++;
+    }
+    return (0);
+}
+
+int ft_draw_type_line(int x0, int y0, int x1, int y1, void *mlx, void *win)
+{
+    if (x1 > y1 && y1 < y0)
+        ft_draw_line_hard(x0, y0, x1, y1, mlx, win);
+    else if (x1 < y1 && y1 > y0)
+        ft_draw_line_hard(x0, y0, x1, y1, mlx, win);
+    else
+        ft_draw_line_slow(x0, y0, x1, y1, mlx, win);
+    return (0);
+}
+
+//mlx_pixel_put(mlx, win, x0 + (dx * (y - y0) / dy), y, 0x00FAAFF);
+/*
+A(-1;4) B(1;0)
+y - y0 = (y1 - y0) / x1 - x0 * (x - x1)
+*/
+int main()
+{
+    void *mlx;
+    void *win;
+    int i;
+    int k;
+
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, 600, 600, "fdf");
+
+    i = 0;
+    while (i < 600)
+    {
+        ft_draw_type_line(300, 300, i, 0, mlx, win);
+        i += 1;
+    }
+    i = 0;
+    while (i < 600)
+    {
+        ft_draw_type_line(300, 300, 600, i, mlx, win);
+        i += 1;
+    }
+    i = 600;
+    while (i > 0)
+    {
+        ft_draw_type_line(300, 300, i, 600, mlx, win);
+        i -= 1;
+    }
+    i = 600;
+    while (i > 0)
+    {
+        ft_draw_type_line(300, 300, 0, i, mlx, win);
+        i -= 1;
+    }
+    mlx_key_hook(win, ft_put_key_num, 0);
+    mlx_key_hook(win, ft_push_exit, 0);
+    mlx_loop(mlx);
+}
+
+/*
 int ft_draw_line_two(int x0, int y0, int x1, int y1, void *mlx, void *win)
 {
     int dx;
@@ -99,99 +187,4 @@ int ft_check_draw(int x0, int y0, int x1, int y1, void *mlx, void *win)
     }
     return (0);
 }
-/*
-A(-1;4) B(1;0)
-y - y0 = (y1 - y0) / x1 - x0 * (x - x1)
 */
-int main()
-{
-    void *mlx;
-    void *win;
-    int i;
-    int k;
-
-    mlx = mlx_init();
-    win = mlx_new_window(mlx, 600, 600, "fdf");
-
-    i = 0;
-    while (i < 600)
-    {
-        ft_check_draw(300, 300, i, 0, mlx, win);
-        i += 1;
-    }
-    i = 0;
-    while (i < 600)
-    {
-        ft_check_draw(300, 300, 600, i, mlx, win);
-        i += 1;
-    }
-    i = 600;
-    while (i > 0)
-    {
-        ft_check_draw(300, 300, i, 600, mlx, win);
-        i -= 1;
-    }
-    i = 600;
-    while (i > 0)
-    {
-        ft_check_draw(300, 300, 0, i, mlx, win);
-        i -= 1;
-    }
-
-//    ft_check_draw(300, 300, 0, 300, mlx, win);
-//    ft_check_draw(300, 300, 300, 0, mlx, win);
-//    ft_check_draw(300, 300, 600, 600, mlx, win);
-//    ft_check_draw(300, 300, 600, 300, mlx, win);
-//    ft_check_draw(300, 300, 600, 0, mlx, win);
-//    ft_check_draw(300, 300, 300, 600, mlx, win);
-//    ft_check_draw(300, 300, 300, 0, mlx, win);
-//    ft_check_draw(300, 300, 0, 300, mlx, win);
-//    ft_check_draw(300, 300, 0, 600, mlx, win);
-
-
-/*    ft_check_draw(0, 10, 10, 0, mlx, win);
-    ft_check_draw(0, 0, 10, 10, mlx, win);
-    ft_check_draw(10, 0, 0, 10, mlx, win);
-    ft_check_draw(10, 10, 0, 0, mlx, win);
-*/
-//    ft_check_draw(10, 60, 10, 160, mlx, win);
-//    ft_check_draw(10, 110, 110, 10, mlx, win); //--
-//    ft_check_draw(10, 110, 60, 10, mlx, win); //--
-/*    ft_check_draw(10, 10, 110, 110, mlx, win); //-a
-    ft_check_draw(10, 10, 125, 110, mlx, win); //-a
-    ft_check_draw(10, 10, 170, 110, mlx, win); //-a
-    ft_check_draw(10, 10, 250, 110, mlx, win); //-a*/
-
-//    ft_check_draw(10, 110, 10, 10, mlx, win);
-//    ft_check_draw(20, 110, 10, 10, mlx, win);
-//    ft_check_draw(30, 110, 10, 10, mlx, win);
-    /*ft_check_draw(60, 110, 10, 10, mlx, win);
-    ft_check_draw(85, 110, 10, 10, mlx, win);
-    ft_check_draw(100, 110, 10, 10, mlx, win);
-    ft_check_draw(110, 110, 10, 10, mlx, win); //-a
-    ft_check_draw(125, 110, 10, 10, mlx, win); //-a
-    ft_check_draw(170, 110, 10, 10, mlx, win); //-a
-    ft_check_draw(250, 110, 10, 10, mlx, win); //-a*/
-
-/*    ft_check_draw(10, 10, 10, 110, mlx, win); //==
-    ft_check_draw(10, 10, 30, 110, mlx, win); //==
-    ft_check_draw(10, 10, 60, 110, mlx, win); //==
-    ft_check_draw(10, 10, 85, 110, mlx, win); //==
-    ft_check_draw(10, 10, 100, 110, mlx, win); //==*/
-
-/*    ft_check_draw(10, 10, 10, 10, mlx, win); //==
-    ft_check_draw(30, 10, 10, 10, mlx, win); //==
-    ft_check_draw(60, 10, 10, 10, mlx, win); //==
-    ft_check_draw(85, 10, 10, 10, mlx, win); //==
-    ft_check_draw(110, 10, 10, 10, mlx, win); //==*/
-
-/*    ft_check_draw(10, 10, 10, 110, mlx, win); //==
-    ft_check_draw(20, 10, 10, 110, mlx, win); //==
-    ft_check_draw(30, 10, 10, 110, mlx, win); //==
-    ft_check_draw(60, 10, 10, 110, mlx, win); //==
-    ft_check_draw(85, 10, 10, 110, mlx, win); //==
-    ft_check_draw(110, 10, 10, 110, mlx, win); //==*/
-    mlx_key_hook(win, ft_put_key_num, 0);
-    mlx_key_hook(win, ft_push_exit, 0);
-    mlx_loop(mlx);
-}
