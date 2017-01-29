e0844c8dfa02691ae9ef1c91fe48da64c48edb8f
Valentin OMNES <vomnes@student.42.fr>
2017-01-29 17:54:20 +0100

    Zoom if ok

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ static int ft_check_min_x(t_point *coord, t_data *data)
     tmp_min = 0;
     while (i < data->map_total_size)
     {
-        tmp_min = sqrt(2)/2 * (coord[i].x - coord[i].y) * data->zoom;
+        tmp_min = sqrt(2) / 2 * (coord[i].x - coord[i].y) * data->zoom;
         min = tmp_min < min ? tmp_min : min;
         i++;
     }
@@ -42,7 +42,7 @@ static int ft_check_max_x(t_point *coord, t_data *data)
     tmp_max = 0;
     while (i < data->map_total_size)
     {
-        tmp_max = sqrt(2)/2 * (coord[i].x - coord[i].y) * data->zoom;
+        tmp_max = sqrt(2) / 2 * (coord[i].x - coord[i].y) * data->zoom;
         max = tmp_max > max ? tmp_max : max;
         i++;
     }
@@ -95,10 +95,10 @@ static void	ft_init_data(t_data *data, int fd)
     data->map_length = ft_max_length(data->input);
     data->map_width = ft_width(data->input);
     data->map_total_size = data->map_length * data->map_width;
-    data->zoom = 80;
+    data->zoom = 5;
     data->pos_x = 0;
     data->pos_y = 0;
-    data->deep = 10;
+    data->deep = 2;
 }
 
 int ft_move_xy(int keycode, t_data *data, void *param);
@@ -107,6 +107,34 @@ int expose_hook(t_point *coord, t_data *data)
 {
     ft_build_3d(coord, data);
     return (0);
+}
+
+void ft_get_zoom(t_data *data)
+{
+    if (data->map_total_size <= 250)
+        data->zoom = 60;
+    else if (data->map_total_size <= 500)
+        data->zoom = 50;
+    else if (data->map_total_size <= 1000)
+        data->zoom = 40;
+    else if (data->map_total_size <= 5000)
+        data->zoom = 30;
+    else if (data->map_total_size <= 10000)
+        data->zoom = 10;
+    else if (data->map_total_size <= 25000)
+        data->zoom = 8;
+    else if (data->map_total_size <= 50000)
+        data->zoom = 5;
+    else if (data->map_total_size <= 100000)
+        data->zoom = 4;
+    else if (data->map_total_size <= 150000)
+        data->zoom = 3;
+    else if (data->map_total_size <= 250000)
+        data->zoom = 2;
+    else if (data->map_total_size <= 500000)
+        data->zoom = 1;
+    else
+        data->zoom = 0.5;
 }
 
 int main(int args, char **argv)
@@ -126,6 +154,7 @@ int main(int args, char **argv)
     if (!(coord = (t_point*)malloc(sizeof(t_point) * data.map_total_size)))
         return (-1);
     ft_coord_in_struct(data.input, coord, data.map_total_size);
+    ft_get_zoom(&data);
     data.pos_x = ft_check_min_x(coord, &data) + 50;
     data.pos_y = ft_check_min_y(coord, &data) + 50;
     data.win_width = ft_check_max_x(coord, &data) + data.pos_x + 50;
@@ -134,6 +163,9 @@ int main(int args, char **argv)
     data.win = mlx_new_window(data.mlx, data.win_width,
     data.win_height, "fdf");
     ft_build_3d(coord, &data);
+    ft_printf("map_length >> %d\n", data.map_length);
+    ft_printf("map_width >> %d\n", data.map_width);
+    ft_printf("map_total_size >> %d\n", data.map_total_size);
     //mlx_key_hook(data.win, ft_print_key_num, 0);
     mlx_key_hook(data.win, ft_push_exit, 0);
 //    mlx_expose_hook(data.win, expose_hook, 0);
